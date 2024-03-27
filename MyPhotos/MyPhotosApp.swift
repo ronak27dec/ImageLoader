5af1188ae7454f85e5c59ec23983456c22c3e2c0
ronak <rmal0008@synchronoss.com>
2024-03-27 23:32:36 +0530

    Initial Commit

---
+++ MyPhotos/MyPhotosApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyPhotosApp.swift
+//  MyPhotos
+//
+//  Created by Ronak Malick on 27/03/24.
+//
+
+import SwiftUI
+
+@main
+struct MyPhotosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
